22aa01d3dcd1d3482f4e92a84013487cad6d1152
Andreas Tscheinig <andreas-tscheinig@gmx.at>
2022-12-09 21:05:00 +0100

    add player shooting and reload state

--- Assets/Scipts/PlayerController.cs
+++ Assets/Scipts/PlayerController.cs
@@ -4,6 +4,38 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float Health;
+    public int BulletsInMagazine;
+
+    public readonly float BaseMaxHealth;
+    public readonly float BaseMovementSpeed;
+    public readonly float BaseRateOfFire;
+    public readonly float BaseDamagePerBullet;
+    public readonly int BaseMagazineSize;
+    public readonly float BaseTimeToReload;
+
+    // public List Items = new List<Item>();
+
+    // Values below are calculated  when a new item is consumed
+    public float MaxHealthModifier = 0;
+    public float MovementSpeedModifier = 0;
+    public float RateOfFireModifier = 0;
+    public float DamagePerBulletModifier = 0;
+    public int MagazineSizeModifier = 0;
+    public float TimeToReloadModifier = 0;
+
+    public float MaxHealth => BaseMaxHealth * MaxHealthModifier;
+    public float MovementSpeed => BaseMovementSpeed * MovementSpeedModifier;
+    public float RateOfFire => BaseRateOfFire * RateOfFireModifier;
+    public float DamagePerBullet => BaseDamagePerBullet * DamagePerBulletModifier;
+    public int MagazineSize => BaseMagazineSize + MagazineSizeModifier;
+    public float TimeToReload => BaseTimeToReload * TimeToReloadModifier;
+
+    /// Internal control variables
+    public bool HasFiringCooldown = false;
+    public bool IsReloading = false;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +46,50 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void Move() {
+
+    }
+
+    void Shoot() {
+        if (IsReloading || HasFiringCooldown) {
+            return;
+        }
+
+        HasFiringCooldown = true;
+
+        BulletsInMagazine--;
+
+        if (BulletsInMagazine == 0) {
+            Reload();
+        }
+
+        ResetWeaponFire();
+    }
+
+    void Reload() {
+        IsReloading = true;
+        ResetReload();
+    }
+
+    IEnumerator ResetWeaponFire() {
+        yield return new WaitForSeconds(RateOfFire);
+        HasFiringCooldown = false;
+    }
+
+    IEnumerator ResetReload (){
+        yield return new WaitForSeconds(TimeToReload);
+        IsReloading = false;
+        BulletsInMagazine = MagazineSize;
+    } 
+
+    void ConsumeItem(Item item) {
+        MaxHealthModifier += item.MaxHealthModifier;
+        MovementSpeedModifier += item.MovementSpeedModifier;
+        RateOfFireModifier += item.RateOfFireModifier;
+        DamagePerBulletModifier += item.DamagePerBulletModifier;
+        MagazineSizeModifier += item.MagazineSizeModifier;
+        TimeToReloadModifier += item.TimeToReloadModifier;
     }
 }
